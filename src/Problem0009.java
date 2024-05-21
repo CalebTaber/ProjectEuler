624c0c6c2c550c6b9f0cb96f2a55dfaa5b5495fd
Caleb Taber <calebtaber@gmail.com>
2024-05-20 20:23:46 -0400

    Problem 9 solved

--- src/Problem0009.java
+++ src/Problem0009.java
@@ -1,7 +1,13 @@
 public class Problem0009 {
 
     public static void main(String[] args) {
-
+        for (int a = 1; a < 1000; a++) {
+            for (int b = a + 1; b < 1000; b++) {
+                for (int c = b + 1; c < 1000; c++) {
+                    if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2) && a + b + c == 1000) System.out.println(a * b * c);
+                }
+            }
+        }
     }
 
 }
